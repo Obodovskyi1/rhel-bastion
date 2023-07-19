a3b72a7e7ef3c0c2645270dd377baaaf389e2abb
Artem Obodovskyi <64009871+Obodovskyi1@users.noreply.github.com>
2023-07-19 19:51:53 +0200

    rhel

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "Updating the operating system. Wait!"
-sudo apt-get update -y >> /dev/null 2>&1
-sudo apt-get upgrade -y >> /dev/null 2>&1
+sudo yum update -y >> /dev/null 2>&1
+sudo yum upgrade -y >> /dev/null 2>&1
 echo "Installing Docker. Wait!"
 # sudo apt-get remove docker docker-engine docker.io containerd runc >> /dev/null 2>&1
 # sudo apt-get update >> /dev/null 2>&1
@@ -13,16 +13,18 @@ echo "Installing Docker. Wait!"
 # echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >> /dev/null 2>&1
 # sudo apt-get update >> /dev/null 2>&1
 # sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin >> /dev/null 2>&1
-sudo apt install docker.io -y >> /dev/null 2>&1
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 echo "Starting Docker. Wait!"
-sudo service docker start >> /dev/null 2>&1
+sudo systemctl start docker
 echo "Adding user to docker group. Wait!"
 sudo usermod -aG docker $USER >> /dev/null 2>&1
 echo "Setting up docker to start at reboot. Wait!"
 sudo systemctl enable docker.service >> /dev/null 2>&1
 sudo systemctl enable containerd.service >> /dev/null 2>&1
 echo "Installing common utilities. Wait!"
-sudo apt-get install -y python3.9 python3-pip sudo dnsutils wget vim gettext iputils-ping mlocate git curl openssl zip unzip jq openjdk-8-jdk >> /dev/null 2>&1
+sudo yum install -y python3.9 python3-pip sudo dnsutils wget vim gettext iputils-ping mlocate git curl openssl zip unzip jq openjdk-8-jdk >> /dev/null 2>&1
 sudo updatedb >> /dev/null 2>&1
 echo "Installing ibmcloud utility. Wait!"
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh >> /dev/null 2>&1
@@ -41,7 +43,7 @@ echo "Installing Kubernetes kubectl. Wait!"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" >> /dev/null 2>&1
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl >> /dev/null 2>&1
 echo "cleaning up. Wait!"
-sudo apt-get clean all -y >> /dev/null 2>&1
+sudo yum clean all -y >> /dev/null 2>&1
 echo "All done."
 oc version --client
 helm version --client
